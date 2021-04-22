c3f1e3f576290ad5e42557ae5e4d5971026307da
ummu2222 <56568688+ummu2222@users.noreply.github.com>
2021-04-22 20:46:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const bg=document.getElementById('bg');
+const frame1=document.getElementById('frame1');
+const text=document.getElementById('text');
+
+window.addEventListener('scroll',function(){
+    const value = window.scrollY;
+    bg.style.top = value + 'px';
+    text.style.top = value - 210 +'px';
+})
